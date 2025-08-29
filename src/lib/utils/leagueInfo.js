4ea8583c09051d57a06ffd9dee083fbf6d78f202
bigmoman76 <70778308+bigmoman76@users.noreply.github.com>
2025-08-29 15:37:28 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "1249051260070404096"; // your league ID
 export const leagueName = "Rhodes Ballers"; // your league name
 export const dues = 60; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
